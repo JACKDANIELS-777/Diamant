b9dbb18bab4275aede27eb497085b18b174423e7
JACKDANIELS-777 <jackiewifi279@gmail.com>
2025-03-03 19:11:30 +0200

    Updated the Lexer.py

--- Lexer/Lexer.py
+++ Lexer/Lexer.py
@@ -1,1 +1,42 @@
-...
+from sly import Lexer
+
+class DiamantLexer(Lexer):
+    tokens = {PRINTF,IMPORT,TK,PRINT,NAME, NUMBER, STRING,TRUE,FALSE, IF, THEN, ELSE, FOR, FUN, TO,RET, ARROW, EQEQ }
+    ignore = '\t '
+
+    literals = { '=', '+', '-', '/', '*', '(', ')', ',', ';','.','[',']','{','}',":"}
+    RET = r'ret'
+    # Define tokens
+    PRINTF=r'printf'
+    PRINT = r'print'
+    IF = r'if'
+    IMPORT = r'import'
+    THEN = r'then'
+    TK=r'tk'
+    ELSE = r'else'
+    FOR = r'for'
+    FUN = r'fun'
+    TO = r'to'
+    TRUE=r'True'
+    FALSE =r'False'
+    ARROW = r'->'
+    NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    #STRING = r'\".*?\"'
+    #STRING = r'"(?:\\.|[^"\\])*"'
+    STRING = r'"(?:\\.|[^"\\])*"|\'(?:\\.|[^\'\\])*\''
+
+    EQEQ = r'=='
+
+    @_(r'\d+')
+    def NUMBER(self, t):
+        t.value = int(t.value)
+        return t
+
+    @_(r'#.*')
+    def COMMENT(self, t):
+        pass
+
+    @_(r'\n+')
+    def newline(self,t ):
+        self.lineno = t.value.count('\n')
+
